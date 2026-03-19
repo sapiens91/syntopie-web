f9384d1d3c8263055d13886a139211115ae7ad43
sapiens91 <47326885+sapiens91@users.noreply.github.com>
2026-03-19 09:53:31 +0100

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,1 +1,5 @@
-
+document.querySelectorAll("[data-include]").forEach(async (el) => {
+  const file = el.getAttribute("data-include");
+  const response = await fetch(file);
+  el.innerHTML = await response.text();
+});
